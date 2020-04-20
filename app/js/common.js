1a985177b20bb27e69dfd1a9254f6b1c510addcc
Barto <illya1004@gmail.com>
2020-04-20 18:32:44 +0300

    Сделал переключатели слайдов с мобильного меню

--- app/js/common.js
+++ app/js/common.js
@@ -4,6 +4,17 @@ let lineOne = document.querySelector(".main-nav__line--1");
 let lineTwo = document.querySelector(".main-nav__line--2");
 let lineThree = document.querySelector(".main-nav__line--3");
 let siteNavigation = document.querySelector(".site-navigation");
+//массив с кнопками меню
+let menuButton = document.querySelectorAll(".site-navigation__link");
+
+
+//Закрытие мобильного меню
+function toggleMenu () {
+  navigation.classList.toggle("main-nav--opened");
+  lineOne.classList.toggle("main-nav__line--cross-1");
+  lineTwo.classList.toggle("main-nav__line--fade-out");
+  lineThree.classList.toggle("main-nav__line--cross-3");
+}
 
 toggleMenuButton.addEventListener("click", () => {
   // siteNavigation.classList.toggle("site-navigation--show");
@@ -16,32 +27,33 @@ toggleMenuButton.addEventListener("click", () => {
 
 //Swiper
 var mySwiper = new Swiper ('.swiper-container', {
+
     // Optional parameters
-    direction: 'vertical',
-    loop: false,
+ // allowTouchMove:true,
+ //    mousewheel: true,
+ //    keyboard: true,
+    direction: 'horizontal',
     // autoHeight: true,
     effect: "fade",
-    speed: 600,
-    forceToAxis: true,
+    speed: 500,
     fadeEffect: { crossFade: true },
-    slidesPerView: 1,
-    spaceBetween: 100,
+    // slidesPerView: 1,
 
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+      el: '.site-navigation',
+      type: 'custom',
+      bulletElement: '.site-navigation__link',
+      clickable: true
+    }
   });
 
- // "fade", "cube", "coverflow" or "flip"
+
+//Навигация по слайдам при клике на элементе меню
+for (let i = 0; i < menuButton.length; i++) {
+  menuButton[i].addEventListener("click", function () {
+    mySwiper.slideTo(i);
+  })
+}
+
+
